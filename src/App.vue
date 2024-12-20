352e200846f276f9a9e9e99076ab2b885de23a21
DaMexx <haron9373@gmail.com>
2024-12-20 17:53:01 +0300

    in progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-<Header />
+  <Header />
+  <main class="main">
+    <section class="design">
+      <h1 class="design__title">
+        <b>Dianne Russell</b>
+        <br />
+        Digital Design
+      </h1>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-
+.design__title {
+  text-align: center;
+  font: 800 40px/65px "Gilroy-medium", sans-serif;
+}
 </style>
